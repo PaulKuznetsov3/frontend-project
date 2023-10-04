c7787182dc13ffd266044dba2d82e9dc38eb5c57
Pavel <pavel_iv37@mail.ru>
2023-10-04 21:23:35 +0300

    delete theme decorator

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++
@@ -1,8 +0,0 @@
-import { Decorator, Story } from '@storybook/react';
-import { Theme } from 'app/provider/ThemeProvider';
-
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Decorator) => (
-    <div className={`app ${theme}`}>
-        <StoryComponent />
-    </div>
-);
